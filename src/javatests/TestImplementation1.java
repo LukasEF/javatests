b30be1269ce23f5a0f8ed063121c2f853810c625
Lukas <Lukas@DESKTOP-BIMO8AU.home>
2021-08-10 13:28:12 +0100

    Interfaces

---
+++ src/javatests/TestImplementation1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatests;
+
+/**
+ *
+ * @author Lukas
+ */
+public class TestImplementation1 implements TestInterface{
+
+    @Override
+    public void print() {
+        System.out.println("TestImplementation1");
+    }
+    
+}
